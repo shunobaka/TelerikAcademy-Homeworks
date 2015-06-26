79e8b7a88e26e0017fd5511d66d2fa11e69bfd0c
shunobaka <vesselin_minev@abv.bg>
2015-06-26 18:46:34 +0300

    Updated the prototypal inheritance homework

--- JS Homeworks/JS OOP/04. Prototypal Inheritance/task-1.js
+++ JS Homeworks/JS OOP/04. Prototypal Inheritance/task-1.js
@@ -1,58 +1,59 @@
 /* Task Description */
 /*
- - * Create an object domElement, that has the following properties and methods:
- - * use prototypal inheritance, without function constructors
- - * method init() that gets the domElement type
- - * i.e. `Object.create(domElement).init('div')`
- - * property type that is the type of the domElement
- - * a valid type is any non-empty string that contains only Latin letters and digits
- - * property innerHTML of type string
- - * gets the domElement, parsed as valid HTML
- - * <type attr1="value1" attr2="value2" ...> .. content / children's.innerHTML .. </type>
- - * property content of type string
- - * sets the content of the element
- - * works only if there are no children
- - * property attributes
- - * each attribute has name and value
- - * a valid attribute has a non-empty string for a name that contains only Latin letters and digits or dashes (-)
- - * property children
- - * each child is a domElement or a string
- - * property parent
- - * parent is a domElement
- - * method appendChild(domElement / string)
- - * appends to the end of children list
- - * method addAttribute(name, value)
- - * throw Error if type is not valid
- * // method removeAttribute(attribute)
+ * Create an object domElement, that has the following properties and methods:
+ * use prototypal inheritance, without function constructors
+ * method init() that gets the domElement type
+ * i.e. `Object.create(domElement).init('div')`
+ * property type that is the type of the domElement
+ * a valid type is any non-empty string that contains only Latin letters and digits
+ * property innerHTML of type string
+ * gets the domElement, parsed as valid HTML
+ * <type attr1="value1" attr2="value2" ...> .. content / children's.innerHTML .. </type>
+ * property content of type string
+ * sets the content of the element
+ * works only if there are no children
+ * property attributes
+ * each attribute has name and value
+ * a valid attribute has a non-empty string for a name that contains only Latin letters and digits or dashes (-)
+ * property children
+ * each child is a domElement or a string
+ * property parent
+ * parent is a domElement
+ * method appendChild(domElement / string)
+ * appends to the end of children list
+ * method addAttribute(name, value)
+ * throw Error if type is not valid
+ * method removeAttribute(attribute)
+ * throw Error if attribute does not exist in the domElement
  */
 
 
 /* Example
 
 var meta = Object.create(domElement)
-	.init('meta')
-	.addAttribute('charset', 'utf-8');
+    .init('meta')
+    .addAttribute('charset', 'utf-8');
 
 var head = Object.create(domElement)
-	.init('head')
-	.appendChild(meta)
+    .init('head')
+    .appendChild(meta)
 
 var div = Object.create(domElement)
-	.init('div')
-	.addAttribute('style', 'font-size: 42px');
+    .init('div')
+    .addAttribute('style', 'font-size: 42px');
 
 div.content = 'Hello, world!';
 
 var body = Object.create(domElement)
-	.init('body')
-	.appendChild(div)
-	.addAttribute('id', 'cuki')
-	.addAttribute('bgcolor', '#012345');
+    .init('body')
+    .appendChild(div)
+    .addAttribute('id', 'cuki')
+    .addAttribute('bgcolor', '#012345');
 
 var root = Object.create(domElement)
-	.init('html')
-	.appendChild(head)
-	.appendChild(body);
+    .init('html')
+    .appendChild(head)
+    .appendChild(body);
 
 console.log(root.innerHTML);
 Outputs:
@@ -98,7 +99,7 @@ function solve() {
                 if (typeof(child) === 'object') {
                     child.parent = this;
                 }
-                
+
                 this.children.push(child);
 
                 return this;
@@ -116,6 +117,22 @@ function solve() {
                 }
 
                 this.attributes.push(attribute);
+
+                return this;
+            },
+            removeAttribute: function(attribute) {
+                var attributeExists = false;
+                for (var att in this.attributes) {
+                    if (this.attributes[att][0] === attribute) {
+                        this.attributes.splice(att, 1);
+                        attributeExists = true;
+                        break;
+                    }
+                }
+
+                if (!attributeExists) {
+                    throw new Error('No such attribute exists!');
+                }
 
                 return this;
             },
@@ -140,7 +157,7 @@ function solve() {
                         if (typeof(this.children[child]) === 'string') {
                             output += this.children[child];
                         } else {
-                        	output += this.children[child].innerHTML;
+                            output += this.children[child].innerHTML;
                         }
                     }
                 }
@@ -164,22 +181,7 @@ function solve() {
         return domElement;
     }());
 
-    var child = Object.create(domElement)
-        .init('child'),
-        parent = Object.create(domElement)
-        .init('parent')
-        .appendChild(child);
-    child.content = 'see me';
-
-    console.log(child);
-    parent.content = 'some random content';
-    console.log(parent.children);
-
-
-
     return domElement;
 }
-
-solve();
 
 module.exports = solve;
