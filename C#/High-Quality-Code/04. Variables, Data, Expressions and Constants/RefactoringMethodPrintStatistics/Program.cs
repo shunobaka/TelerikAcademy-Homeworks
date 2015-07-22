3196dab37113aef5090d4e88516ddf1ed1fe33b0
shunobaka <vesselin_minev@abv.bg>
2015-07-22 21:19:38 +0300

    Added homework for Variables, Data, Expressions and Constants

---
+++ C#/High-Quality-Code/04. Variables, Data, Expressions and Constants/RefactoringMethodPrintStatistics/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace RefactoringMethodPrintStatistics
+{
+    public class Printer
+    {
+        public static void Main()
+        {
+        }
+
+        public void PrintStatistics(double[] values, int count)
+        {
+            double maxValue = 0;
+            for (int index = 0; index < count; index++)
+            {
+                if (values[index] > maxValue)
+                {
+                    maxValue = values[index];
+                }
+            }
+
+            this.PrintMax(maxValue);
+
+            double minValue = 0;
+            for (int index = 0; index < count; index++)
+            {
+                if (values[index] < minValue)
+                {
+                    minValue = values[index];
+                }
+            }
+
+            this.PrintMin(minValue);
+
+            double averageValue = 0;
+            for (int index = 0; index < count; index++)
+            {
+                averageValue += values[index];
+            }
+
+            this.PrintAverage(averageValue / count);
+        }
+
+        private void PrintMax(double max)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void PrintMin(double max)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void PrintAverage(double average)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ C#/High-Quality-Code/04. Variables, Data, Expressions and Constants/RefactoringSizeClass/Size.cs
@@ -0,0 +1,28 @@
+﻿namespace RefactoringSizeClass
+{
+    using System;
+
+    public class Size
+    {
+        private double width;
+        private double height;
+
+        public Size(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public static Size GetRotatedSize(Size size, double rotationAngle)
+        {
+            double absoluteCosRotationAngle = Math.Abs(Math.Cos(rotationAngle));
+            double absoluteSinRotationAngle = Math.Abs(Math.Sin(rotationAngle));
+            double rotatedWidth = (absoluteCosRotationAngle * size.width) + (absoluteSinRotationAngle * size.height);
+            double rotatedHeight = (absoluteSinRotationAngle * size.width) + (absoluteCosRotationAngle * size.height);
+
+            Size rotatedSize = new Size(rotatedWidth, rotatedHeight);
+
+            return rotatedSize;
+        }
+    }
+}
