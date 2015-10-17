dbfbc3b2ec903746d34b2d4e97bd9bc6ea15f8ce
shunobaka <vesselin_minev@abv.bg>
2015-10-17 22:52:19 +0300

    Uploading homework for Mocking and JustMock

---
+++ C#/High-Quality-Code/15. Mocking and JustMock/Cars.Tests.JustMock/Mocks/JustMockCarsRepository.cs
@@ -0,0 +1,32 @@
+﻿namespace Cars.Tests.JustMock.Mocks
+{
+    using System.Linq;
+
+    using Cars.Contracts;
+    using Cars.Models;
+
+    using Telerik.JustMock;
+
+    public class JustMockCarsRepository : CarRepositoryMock, ICarsRepositoryMock
+    {
+        protected override void ArrangeCarsRepositoryMock()
+        {
+            this.CarsData = Mock.Create<ICarsRepository>();
+            Mock.Arrange(() => this.CarsData.Add(Arg.IsAny<Car>())).DoNothing();
+
+            Mock.Arrange(() => this.CarsData.All()).Returns(this.FakeCarCollection);
+
+            Mock.Arrange(() => this.CarsData.Search(Arg.AnyString))
+                .Returns((string search) =>
+                    this.FakeCarCollection
+                        .Where(c => c.Make == search || c.Model == search)
+                        .ToList());
+            Mock.Arrange(() => this.CarsData.Search(Arg.NullOrEmpty)).Returns(() => this.FakeCarCollection.ToList());
+
+            Mock.Arrange(() => this.CarsData.GetById(Arg.AnyInt)).Returns((int id) => this.FakeCarCollection.FirstOrDefault(car => car.Id == id));
+
+            Mock.Arrange(() => this.CarsData.SortedByMake()).Returns(() => this.FakeCarCollection.OrderBy(car => car.Make).ToList());
+            Mock.Arrange(() => this.CarsData.SortedByYear()).Returns(() => this.FakeCarCollection.OrderBy(car => car.Year).ToList());
+        }
+    }
+}
---
+++ C#/High-Quality-Code/15. Mocking and JustMock/Cars.Tests.JustMock/Mocks/MoqCarsRepository.cs
@@ -0,0 +1,29 @@
+﻿namespace Cars.Tests.JustMock.Mocks
+{
+    using Cars.Contracts;
+    using Cars.Models;
+    using Moq;
+    using System.Linq;
+
+    public class MoqCarsRepository : CarRepositoryMock, ICarsRepositoryMock
+    {
+        protected override void ArrangeCarsRepositoryMock()
+        {
+            var mockedCarsRepository = new Mock<ICarsRepository>();
+            mockedCarsRepository.Setup(r => r.Add(It.IsAny<Car>())).Verifiable();
+
+            mockedCarsRepository.Setup(r => r.All()).Returns(this.FakeCarCollection);
+
+            mockedCarsRepository.Setup(r => r.Search(It.IsNotNull<string>())).Returns((string search) => 
+                this.FakeCarCollection
+                    .Where(c => c.Make == search || c.Model == search).ToList());
+            mockedCarsRepository.Setup(r => r.Search(It.Is<string>(s => string.IsNullOrEmpty(s)))).Returns(() => this.FakeCarCollection.ToList());
+
+            mockedCarsRepository.Setup(r => r.GetById(It.IsAny<int>())).Returns((int id) => this.FakeCarCollection.FirstOrDefault(car => car.Id == id));
+
+            mockedCarsRepository.Setup(r => r.SortedByMake()).Returns(() => this.FakeCarCollection.OrderBy(car => car.Make).ToList());
+            mockedCarsRepository.Setup(r => r.SortedByYear()).Returns(() => this.FakeCarCollection.OrderBy(car => car.Year).ToList());
+            this.CarsData = mockedCarsRepository.Object;
+        }
+    }
+}
